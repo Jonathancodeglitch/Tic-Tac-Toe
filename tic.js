a7b0a340315f64324d730d4efe7bec4d8ae9ec0f
Jonathan <jonathankendrick697@gmail.com>
2023-08-31 12:18:29 -0700

    first refactor

--- tic.js
+++ tic.js
@@ -6,15 +6,19 @@
 //create an ai so a player can play with a computer
 
 let cells = document.querySelectorAll('.box');
-let currentPlayer = document.getElementById('current-player');
+
 let modal = document.getElementById('modal');
 let modalText = document.querySelector('.display-wins');
 let restartButton = document.querySelector('.restart');
 
-let PlayerOneTurn = true;
-
 const gameBoard = (() => {
-  const board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+  const board = [];
+
+  const addMarker = (cellValue) => {
+    gameController.getCurrentPlayerTurn() == playerOne.name
+      ? (board[cellValue] = 'X')
+      : (board[cellValue] = 'O');
+  };
 
   const getBoard = () => {
     return board;
@@ -38,9 +42,13 @@ const gameBoard = (() => {
 
   return {
     getBoard,
+    addMarker,
     winCondition,
   };
 })();
+
+//get players names from localstorage;
+let playerNames = JSON.parse(localStorage.getItem('playerNames'));
 
 //player object
 const player = (name, marker) => {
@@ -48,13 +56,19 @@ const player = (name, marker) => {
   return { name, marker, moves };
 };
 
-let playerOne = player('morgan', 'x');
-let playerTwo = player('Ada', 'o');
+let playerOne = player(playerNames.playerOne, 'x');
+let playerTwo = player(playerNames.playerTwo, 'o');
 
 //game conttroller
 const gameController = (() => {
+  let PlayerOneTurn = true;
+
   const changePlayerTurn = () => {
     PlayerOneTurn = !PlayerOneTurn;
+  };
+
+  const getCurrentPlayerTurn = () => {
+    return PlayerOneTurn ? playerOne.name : playerTwo.name;
   };
 
   const updatePlayerMoves = (value) => {
@@ -64,6 +78,7 @@ const gameController = (() => {
       playerTwo.moves.push(Number(value));
     }
   };
+
   //check win
   const checkWin = () => {
     let winCondition = gameBoard.winCondition();
@@ -86,6 +101,7 @@ const gameController = (() => {
     //continue from here
     return [winnerName, winnerMoves];
   };
+
   // check if cell is empty
   const checkForEmptyCell = () => {
     let isCellEmpty = true;
@@ -97,6 +113,12 @@ const gameController = (() => {
     return isCellEmpty;
   };
 
+  //play round
+  const playRound = (cellValue) => {
+    gameBoard.addMarker(cellValue);
+    changePlayerTurn();
+    updatePlayerMoves(cellValue);
+  };
   //restate game
   const restateGame = () => {
     //set player turn back to default
@@ -115,23 +137,22 @@ const gameController = (() => {
   };
 
   return {
-    changePlayerTurn,
+    getCurrentPlayerTurn,
     checkWin,
     checkForEmptyCell,
-    updatePlayerMoves,
     restateGame,
+    playRound,
   };
 })();
 
 //display obj
 const displayController = (() => {
-  // let board = gameBoard.getBoard();
+  const board = gameBoard.getBoard();
   // let count = 0;
+  const currentPlayer = document.getElementById('current-player');
 
   const displayCurrentPlayerName = () => {
-    let text = `It's ${
-      PlayerOneTurn == true ? playerOne.name : playerTwo.name
-    } Turn`;
+    let text = `It's ${gameController.getCurrentPlayerTurn()} Turn`;
     currentPlayer.textContent = text;
   };
 
@@ -160,13 +181,18 @@ const displayController = (() => {
   const displayMarker = (e) => {
     let cell = e.target;
     let cellValue = cell.dataset.value;
+
     if (cell.textContent == '') {
       //update gamboard with x or o
-      cell.textContent = `${PlayerOneTurn ? 'X' : 'O'}`;
-      gameController.updatePlayerMoves(cellValue);
-      gameController.changePlayerTurn();
+      cell.textContent = `${
+        gameController.getCurrentPlayerTurn() == playerOne.name ? 'X' : 'O'
+      }`;
+
+      gameController.playRound(cellValue);
       displayCurrentPlayerName();
       displayWinner();
+
+      console.log(board);
     }
   };
 
